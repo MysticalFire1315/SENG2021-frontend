067849bf588787dffd8ed6abfd0b210f608a8ee3
derpyhead999 <70264124+derpyhead999@users.noreply.github.com>
2023-03-23 16:16:45 +1100

    developed frontend and backend

--- seng-frontend/src/App.js
+++ seng-frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Navbar from './components/NavbarElements';
+import MetaComponents from './components/Metadata';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MetaComponents />
+      <Navbar />
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ seng-frontend/src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <header>This is my header</header>
+    );
+}
+export default Header;
