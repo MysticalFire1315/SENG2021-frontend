11488dbc24a72d57aeb06021a1e03c925aebe75e
Johnny <qinjohnny1@gmail.com>
2023-03-24 02:34:02 +1100

    developed frontend with header and footer basic

--- seng-frontend/src/components/Header.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-const Header = () => {
-    return (
-        <header>This is my header</header>
-    );
-}
-export default Header;
--- seng-frontend/src/components/Main.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-const Main = () => {
-    return (
-        <main>This is a main</main>
-    );
-}
-export default Main;
