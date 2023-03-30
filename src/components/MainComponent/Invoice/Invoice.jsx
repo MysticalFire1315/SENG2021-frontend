8fb89adeaf3956b168e0e5cad115a64e34ea9003
Johnny <qinjohnny1@gmail.com>
2023-03-30 16:22:49 +1100

    got the adding invoice item working

--- src/components/MainComponent/Invoice/Invoice.jsx
+++ src/components/MainComponent/Invoice/Invoice.jsx
@@ -1,27 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InvoiceItem from "./InvoiceItem";
 import { v4 as uuidv4 } from 'uuid';
 const Invoice = () => {
     const [itemIds, setItemIds] = useState([]);
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState([]);         // If there are no E-invoice items, the E-invoice validator must check there is at least 1 invoice line
+
+    useEffect(() => {
+        console.log(itemIds);
+    }, [itemIds]);
+
     const handleAddItem = (e) => {
         e.preventDefault();
         const itemId = uuidv4();
         setItemIds([...itemIds, itemId]);
         setItems([...items, <InvoiceItem key={itemId} id={itemId} onRemove={handleRemoveItem} />]);
-        console.log(itemIds);
     };
     const handleRemoveItem = (id) => {
-        if (items.length == 0) {
+        if (itemIds.length == 0) {
             return;
         }
-        console.log(itemIds);
-        const updatedItemIds = itemIds.filter(itemId => itemId !== id);
+        const updatedItemIds = (itemIds) => itemIds.filter(itemId => itemId !== id);
         setItemIds(updatedItemIds);
-        const updatedItems = items.filter(item => item.props.id !== id);
+        const updatedItems = (items) => items.filter(item => item.props.id !== id);
         setItems(updatedItems);
     }
-
 
     return (
         <div id="invoice-box">
@@ -29,24 +31,31 @@ const Invoice = () => {
                 <h2>INVOICE</h2>
                 <p>TESTID:12570513571</p>
             </div>
-            <div id="invoice-buyer">
-                <h4 className="invoice-headers">Buyer details</h4>
-                <p>Detail 1: Input 1</p>
-                <p>Detail 2: Input 2</p>
-                <p>Detail 3: Input 3</p>
-            </div>
-            <div id="invoice-seller">
-                <h4 className="invoice-headers">Seller details</h4>
-                <p>Detail 1: Input 1</p>
-                <p>Detail 2: Input 2</p>
-                <p>Detail 3: Input 3</p>
-            </div>
-            <div id="invoice-date">
-                <h4 className="invoice-headers">Date details</h4>
-                <p>Detail 1: Input 1</p>
-                <p>Detail 2: Input 2</p>
-                <p>Detail 3: Input 3</p>
-            </div>
+            <form>
+                <div id="invoice-buyer">
+                    <h4 className="invoice-headers">Buyer details</h4>
+                    <div class="row g-3 align-items-center" style={{ width: "200px", borderColor: "transparent" }}>
+                        <label for="invoice-buyer-details" class="col-sm-2 col-form-label">Detail-1</label>
+                        <input type="text" class="form-control-sm form-text" id="invoice-detail-1" aria-labelledby="invoice-detail-1" placeholder="deet 1" />
+                        <label for="invoice-buyer-details" class="col-sm-2 col-form-label">Detail-2</label>
+                        <input type="text" class="form-control-sm form-text" id="invoice-detail-2" aria-labelledby="invoice-detail-2" placeholder="deet 2" />
+                        <label for="invoice-buyer-details" class="col-sm-2 col-form-label">Detail-3</label>
+                        <input type="text" class="form-control-sm form-text" id="invoice-detail-3" aria-labelledby="invoice-detail-3" placeholder="deet 3" />
+                    </div>
+                </div>
+                <div id="invoice-seller">
+                    <h4 className="invoice-headers">Seller details</h4>
+                    <p>Detail 1: Input 1</p>
+                    <p>Detail 2: Input 2</p>
+                    <p>Detail 3: Input 3</p>
+                </div>
+                <div id="invoice-date">
+                    <h4 className="invoice-headers">Date details</h4>
+                    <p>Detail 1: Input 1</p>
+                    <p>Detail 2: Input 2</p>
+                    <p>Detail 3: Input 3</p>
+                </div>
+            </form>
             <div id="invoice-item-list">
                 <div id="invoice-item-tab">
                     <ul className="invoice-elements">
