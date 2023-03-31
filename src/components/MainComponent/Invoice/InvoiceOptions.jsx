368baef65c7c5e8ff718f79a5ab43ebfd9d2a8f2
derpyhead999 <70264124+derpyhead999@users.noreply.github.com>
2023-03-31 12:55:25 +1100

    worked on invoice download

--- src/components/MainComponent/Invoice/InvoiceOptions.jsx
+++ src/components/MainComponent/Invoice/InvoiceOptions.jsx
@@ -4,7 +4,9 @@ import jsPDF from "jspdf";
 const InvoiceOptions = () => {
 
     const generatePDF = () => {
-        const report = new jsPDF('portrait', 'pt', 'A4');
+        const report = new jsPDF('l', 'mm', 'a4');
+        let width = report.internal.pageSize.getWidth()
+        let height = report.internal.pageSize.getHeight()
         report.html(document.querySelector('#invoice-box')).then(() => {
             report.save('invoice.pdf');
         });
@@ -13,9 +15,9 @@ const InvoiceOptions = () => {
 
     return (
         <div id="invoice-options">
-            <button type="button" onClick={generatePDF} id="export-button">Download My Invoice</button>
-            <button id="download-button">Preview My Invoice</button>
-            <button id="save-button">Save My Invoice</button>
+            <button type="button" onClick={generatePDF} class="btn btn-secondary btn-sm">Download My Invoice</button>
+            <button type="button" class="btn btn-secondary btn-sm">Preview My Invoice</button>
+            <button type="button" class="btn btn-secondary btn-sm">Save My Invoice</button>
             <div class="row" className="fileInput">
                 <label for="formFileSm" class="form-label">Input a file (JSON, YAML, XML)</label>
                 <input class="form-control form-control-sm" id="formFileSm" type="file" />
