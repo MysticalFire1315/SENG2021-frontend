51833699109131a97e30c68317f99660ae95d10e
Johnny <qinjohnny1@gmail.com>
2023-04-03 01:39:39 +1000

    removed upload file mechanism for further development

--- src/components/MainComponent/Invoice/InvoiceOptions.jsx
+++ src/components/MainComponent/Invoice/InvoiceOptions.jsx
@@ -5,10 +5,10 @@ const InvoiceOptions = () => {
             <button type="button" class="btn btn-secondary btn-sm">Download My Invoice</button>
             <button type="button" class="btn btn-secondary btn-sm">Create My Invoice</button>
             <button type="button" class="btn btn-secondary btn-sm">Save My Invoice</button>
-            <div class="row" className="fileInput">
+            {/* <div class="row" className="fileInput">
                 <label for="formFileSm" class="form-label">Upload a file (.JSON, .YAML, .XML)</label>
                 <input class="form-control form-control-sm" id="formFileSm" type="file" />
-            </div>
+            </div> */}
         </div>
     );
 }
