2be1cf84771d95e967927e376a73474fb66676b5
derpyhead999 <70264124+derpyhead999@users.noreply.github.com>
2023-04-14 11:36:14 +1000

    changed fontsize

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -11,40 +11,40 @@ const Carousel = () => {
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <div class="carousel-caption d-none d-md-block">
-                        <h2 style={{
+                        <h3 style={{
                             color: "#243762", fontFamily: "Trebuchet MS, Lucida Sans Unicode"
-                        }}>&quot;I never thought that invoice transactions could be made so simple!&quot;</h2>
+                        }}>&quot;I never thought that invoice transactions could be made so simple!&quot;</h3>
                         <br></br>
                         <br></br>
                         <br></br>
-                        <h4 style={{
+                        <h5 style={{
                             color: "#243762", fontFamily: "Trebuchet MS, Lucida Sans Unicode"
-                        }}>Ada Lovelace</h4>
+                        }}>Ada Lovelace</h5>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <div class="carousel-caption d-none d-md-block">
-                        <h2 style={{
+                        <h3 style={{
                             color: "#243762", fontFamily: "Trebuchet MS, Lucida Sans Unicode"
-                        }}>&quot;From creating invoices for me, letting me store them, saving them for later. What else can I say?&quot;</h2>
+                        }}>&quot;From creating invoices for me, letting me store them, saving them for later. What else can I say?&quot;</h3>
                         <br></br>
                         <br></br>
-                        <h4 style={{
+                        <h5 style={{
                             color: "#243762", fontFamily: "Trebuchet MS, Lucida Sans Unicode"
-                        }}>John Smith</h4>
+                        }}>John Smith</h5>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <div class="carousel-caption d-none d-md-block">
-                        <h2 style={{
+                        <h3 style={{
                             color: "#243762", fontFamily: "Trebuchet MS, Lucida Sans Unicode"
-                        }}>&quot;They always respond so quickly to any concerns I have. That&apos;s always great.&quot;</h2>
+                        }}>&quot;They always respond so quickly to any concerns I have. That&apos;s always great.&quot;</h3>
                         <br></br>
                         <br></br>
                         <br></br>
-                        <h4 style={{
-                            color: "#243762", fontFamily: "Trebuchet MS, Lucida Sans Unicode"
-                        }}>Bill Jones</h4>
+                        <h5 style={{
+                            color: "#243762", fontFamily: "Trebuchet MS, Lucida Sans Unicode",
+                        }}>Bill Jones</h5>
                     </div>
                 </div>
             </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <header class="header">
             <h1 style={{ fontSize: "50px" }}>Elegant Builders</h1>
-            <p style={{ color: "#243762", fontWeight: "normal", marginTop: "20px" }}>Bringing the responsive E-invoice creation service directly into the professional&apos;s hands...</p>
+            <p style={{ color: "#243762", fontWeight: "normal", marginTop: "15px", fontSize: "20px" }}>Bringing the responsive E-invoice creation service directly into the professional&apos;s hands...</p>
         </header>
     );
 }
