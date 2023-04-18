667ae1d092ffbf0be13f415406d9e2151df8da38
Jaiden <z5421748@ad.unsw.edu.au>
2023-04-19 00:42:45 +1000

    corrected downloadInvocie implementation

--- src/components/MainComponent/Invoice/InvoiceOptions.jsx
+++ src/components/MainComponent/Invoice/InvoiceOptions.jsx
@@ -20,15 +20,17 @@ const InvoiceOptions = (props) => {
         }
     };
 
-    const downloadInvoice = async () => {   
-        const pdf = new jsPDF("portrait", "pt", "a4"); 
-        const data = await html2canvas(document.querySelector("#invoice-box"));
-        const img = data.toDataURL("image/png");  
-        const imgProperties = pdf.getImageProperties(img);
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
-        pdf.addImage(img, "PNG", 0, 0, pdfWidth, pdfHeight);
-        pdf.save("invoice.pdf");
+    const downloadInvoice = () => {
+        const xmlString = '<invoice><number>123</number><total>100.00</total></invoice>';
+        const xmlBlob = new Blob([xmlString], { type: 'text/xml' });
+        const xmlUrl = URL.createObjectURL(xmlBlob);
+        const link = document.createElement('a');
+        link.href = xmlUrl;
+        link.download = 'invoice.xml';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(xmlUrl);
       };
 
     return (
