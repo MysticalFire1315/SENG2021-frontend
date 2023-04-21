c672a2cb7e575c2ae56dcefaf031513fe12abcfa
Fai Chan <z5411219@ad.unsw.edu.au>
2023-04-21 11:51:13 +1000

    added to help page

--- src/pages/NeedHelp.jsx
+++ src/pages/NeedHelp.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import NavHeader from '../components/NavbarElements';
 import Footer from '../components/Footer/Footer';
 import Head from 'next/head';
+import Image from 'next/image';
 
 export default function NeedHelp() {
   const [showInvoiceIds, setShowInvoiceIds] = useState(false);
+  const [showCreate, setCreate] = useState(false);
   const [showNavigation, setShowNavigation] = useState(false);
   const [showPOnumber, setShowPOnumber] = useState(false);
   const [showRender, setShowRender] = useState(false);
@@ -55,11 +57,11 @@ export default function NeedHelp() {
             marginBottom: '10px',
             paddingLeft: '30px',
           }}
-          onClick={() => setShowInvoiceIds(!showInvoiceIds)}
+          onClick={() => setCreate(!showCreate)}
         >
           Formatting for Invoice IDs
         </h4>
-        {showInvoiceIds && (
+        {showCreate && (
           <p
             style={{
               fontSize: '1rem',
@@ -70,6 +72,61 @@ export default function NeedHelp() {
           >
             To format an Invoice ID, click &quot;Other Sites&quot; and then
             &quot;See examples&quot; to view examples of an invoice.
+          </p>
+        )}
+      </section>
+
+      <section>
+        <h4
+          style={{
+            fontSize: '1.5rem',
+            fontWeight: 'bold',
+            color: '#708090',
+            marginBottom: '10px',
+            paddingLeft: '30px',
+          }}
+          onClick={() => setShowInvoiceIds(!showInvoiceIds)}
+        >
+          Creating an invoice
+        </h4>
+        {showInvoiceIds && (
+          <p
+            style={{
+              fontSize: '1rem',
+              color: '#696969',
+              marginBottom: '10px',
+              paddingLeft: '30px',
+            }}
+          >
+            To create an invoice, fill out the form with your required details, then click render invoice to view your completed invoice.
+            <br></br>
+            <Image
+              src="/render.png"
+              alt="render.png"
+              width={983}
+              height={752}
+              style={{ marginBottom: '40px'}}
+            />
+            <br></br>
+            <Image
+              src="/rendered.png"
+              alt="rendered.png"
+              width={1064}
+              height={661}
+              style={{ marginBottom: '40px' }}
+            />
+            <br></br>
+            Alternatively, one can choose to upload a file in the correct format as <a href="http://seng2021-f12a-api-env.eba-pymctycp.ap-southeast-2.elasticbeanstalk.com/docs/guide">documented</a>
+              here. Then, users can upload a JSON, YML or XML file, and the formatted invoice will be downloaded automatically.
+             <br></br>
+            <Image
+              src="/download.png"
+              alt="download.png"
+              width={977}
+              height={760}
+              style={{ marginBottom: '40px' }}
+            />
+             
           </p>
         )}
       </section>
